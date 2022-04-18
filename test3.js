26e1239b3c4237ad0cc46dbadefd6e8efe993c91
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-04-18 22:55:33 +0600

    add test 3

---
+++ test3.js
@@ -0,0 +1,3 @@
+// print a string for every item of the array below. String will be like this: "{size} Number" If the number is less than 3, value will be "small", if less than 7, greater than or equal to 3, value will be "medium" , else value will be big.
+// For example , for 3, String will be "medium number.", for 8, it will be "large number"
+const flag = [ 1,3,2,6,5,8,7,9,2]
