6db3ddc35d6f8d1ff4f49087d6d5a04293a9a060
Tarmim Hoque Shuvo <tarmimhoqueshuvo@gmail.com>
2022-05-01 14:43:25 +0600

    add test 5

---
+++ test5.js
@@ -0,0 +1,2 @@
+// part 1: create a counter which returns wrong count :P .Add a random number to the count and print the count every second. initial value is 0.
+// part 2: custom hook. create a hook which returns the previous value of a variable. suppose the counter in part 1 has values like this: 0,3,10,14 for first 4 seconds. so for first 4 seconds , the hook will return undefined,0,3,10
